fdaa25a4081dd16320b1980228e9dd65b78abe40
Admin <admin@Mac-Admin.local>
2017-12-11 00:27:24 +0300

    UIMapView and pin

---
+++ src/Client/Autostop.Client.iOS/CustomUINavigationBar.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Autostop.Client.iOS
+{
+    public partial class CustomUINavigationBar : UINavigationItem
+    {
+        public CustomUINavigationBar (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ src/Client/Autostop.Client.iOS/MainMapViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Autostop.Client.iOS
+{
+    public partial class MainMapViewController : UIViewController
+    {
+        public MainMapViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
