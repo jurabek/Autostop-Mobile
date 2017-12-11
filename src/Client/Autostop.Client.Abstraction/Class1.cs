11b436d5d1ff33a112eb0c84109e6f395ac0b996
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-12-11 17:01:29 +0200

    ViewModels

---
+++ src/Client/Autostop.Client.Abstraction/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Autostop.Client.Abstraction
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Client/Autostop.Client.Core/ViewModels/Passenger/SelectLocationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autostop.Client.Core.ViewModels.Passenger
+{
+    abstract class SelectLocationViewModel
+    {
+    }
+}
