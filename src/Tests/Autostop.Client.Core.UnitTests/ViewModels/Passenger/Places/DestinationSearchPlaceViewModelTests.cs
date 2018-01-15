91d31a5353fbf7597a15e8eddc5a61985a1c0662
Jurabek Azizkhujaev <jurabek.azizkhujaev@gmail.com>
2018-01-16 01:49:15 +0200

    tests

---
+++ src/Tests/Autostop.Client.Core.UnitTests/ViewModels/Passenger/Places/DestinationSearchPlaceViewModelTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Concurrency;
+using System.Text;
+using AutoFixture;
+using Autostop.Client.Abstraction.Factories;
+using Autostop.Client.Abstraction.Models;
+using Autostop.Client.Abstraction.Providers;
+using Autostop.Client.Abstraction.Services;
+using Autostop.Client.Abstraction.ViewModels;
+using Autostop.Client.Core.Models;
+using Autostop.Client.Core.ViewModels.Passenger;
+using Autostop.Client.Core.ViewModels.Passenger.Places;
+using Moq;
+using NUnit.Framework;
+
+namespace Autostop.Client.Core.UnitTests.ViewModels.Passenger.Places
+{
+    public class DestinationSearchPlaceViewModelTests : BaseAutoMockedReactiveTest<DestinationSearchPlaceViewModel>
+    {
+	    [Test, AutoDomainData]
+	    public void Search_ReturnsHomeAndWorkResultModel_WhenTextSearchIsEmpty(HomeResultModel homeResultModel, WorkResultModel workResultModel, SetLocationOnMapResultModel setLocationOnMapResultModel)
+	    {
+		    // Act 
+		    var viewModel = ClassUnderTest;
+		    var autoCompleteResultProviderMoq = GetMock<IEmptyAutocompleteResultProvider>();
+		    autoCompleteResultProviderMoq.Setup(x => x.GetHomeResultModel()).Returns(homeResultModel);
+		    autoCompleteResultProviderMoq.Setup(x => x.GetWorkResultModel()).Returns(workResultModel);
+		    autoCompleteResultProviderMoq.Setup(x => x.GetSetLocationOnMapResultModel()).Returns(setLocationOnMapResultModel);
+
+			// Arrange
+			Scheduler.Schedule(() => viewModel.SearchText = string.Empty);
+		    Scheduler.Start();
+		    var result = viewModel.SearchResults;
+
+		    // Assert
+		    Assert.AreEqual(homeResultModel, result[0]);
+		    Assert.AreEqual(workResultModel, result[1]);
+		    Assert.AreEqual(setLocationOnMapResultModel, result[2]);
+		}
+
+		[Test, AutoDomainData]
+		public void SelectedSearchResult_NavigatesToChoseDestinationOnMapViewModel_WhenResultViewModelEqualToSetLocationOnMapResultModel(Fixture fixture)
+		{
+			// Arrange
+			var viewModel = ClassUnderTest;
+			var choseDetinationViewModel = Mocker.Create<ChooseDestinationOnMapViewModel>();
+			var setLocationOnMapResultModel = fixture.Create<SetLocationOnMapResultModel>();
+			var homeResultModel = fixture.Create<HomeResultModel>();
+			var workResultModel = fixture.Create<WorkResultModel>();
+			viewModel.SearchResults = new ObservableCollection<IAutoCompleteResultModel>
+			{
+				setLocationOnMapResultModel,
+				homeResultModel,
+				workResultModel
+			};
+
+			GetMock<IChooseOnMapViewModelFactory>().Setup(x => x.GetChooseDestinationOnMapViewModel(It.IsAny<IRideViewModel>()))
+				.Returns(choseDetinationViewModel);
+
+			// Act
+			Scheduler.Schedule(() => viewModel.SelectedSearchResult = viewModel.SearchResults[0]);
+			Scheduler.Start();
+
+			// Assert
+			GetMock<INavigationService>().Verify(x => x.NavigateTo(choseDetinationViewModel), Times.Once);
+		}
+
+		[Test]
+	    public void SelectedSearchResult_NavigatesToAddressViewModel_WhenResultModelsAddressIsNull()
+	    {
+		    // Act
+		    var viewModel = ClassUnderTest;
+		    var homeResultModel = new HomeResultModel(null);
+		    var workResultModel = new WorkResultModel(null);
+		    viewModel.SearchResults = new ObservableCollection<IAutoCompleteResultModel>
+		    {
+			    homeResultModel,
+			    workResultModel
+		    };
+		    var navigationServiceMoq = GetMock<INavigationService>();
+
+		    var searchHomeViewModel = Mocker.Create<SearchHomeAddressViewModel>();
+		    var searchWorkViewModel = Mocker.Create<SearchWorkAddressViewModel>();
+
+		    navigationServiceMoq.Setup(x => x.NavigateToSearchView(It.IsAny<Action<SearchHomeAddressViewModel>>()))
+			    .Callback<Action<SearchHomeAddressViewModel>>(model =>
+			    {
+				    model(searchHomeViewModel);
+			    });
+
+		    navigationServiceMoq.Setup(x => x.NavigateToSearchView(It.IsAny<Action<SearchWorkAddressViewModel>>()))
+			    .Callback<Action<SearchWorkAddressViewModel>>(model =>
+			    {
+				    model(searchWorkViewModel);
+			    });
+
+		    // Arrange
+		    Scheduler.Schedule(() => viewModel.SelectedSearchResult = viewModel.SearchResults[0]);
+		    Scheduler.Schedule(() => viewModel.SelectedSearchResult = viewModel.SearchResults[1]);
+		    Scheduler.Start();
+
+		    // Assert
+		    navigationServiceMoq.Verify(x => x.NavigateToSearchView(It.IsAny<Action<SearchHomeAddressViewModel>>()), Times.Once);
+		    navigationServiceMoq.Verify(x => x.NavigateToSearchView(It.IsAny<Action<SearchWorkAddressViewModel>>()), Times.Once);
+	    }
+	}
+}
